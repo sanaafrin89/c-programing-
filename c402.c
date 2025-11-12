0e587c69dcd7886d49aaeb7c5237d529803c08e6
sanaafrin89 <sanaafrin3712@gmail.com>
2025-11-12 14:33:43 +0530

    Add files via upload

---
+++ c402.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+int main(){
+    int a;
+
+    printf("enter a digit");
+    scanf("%d", &a);
+
+     if(a==1){
+        printf("one");
+    }else if(a==2){
+        printf("two");
+    }else if (a==3){
+        printf("three");
+
+    }else if (a==4){
+        printf("four");
+
+    }else if (a==5){
+        printf("five");
+
+    }else if(a==6){
+        printf("six");
+
+    }else if (a==7){
+        printf("seven");
+
+    }else if(a==8){
+        printf("eight");
+
+    }else if (a==9){
+        printf("nine");
+
+    }else if (a==0){
+        printf("zero");
+
+    }else{
+        printf("invalid digit");
+    }
+
+
+
+}
+
+
+
