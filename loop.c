0fa26cd85f04200f326eef238f449aff406fdddf
sanaafrin89 <sanaafrin3712@gmail.com>
2025-11-12 14:36:43 +0530

    Add files via upload

---
+++ loop.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    int a;
+
+
+    a=1;
+
+    while(a<=5){
+        printf("%d",a);
+        a=a+1;
+    }
+
+
+}
---
+++ loop2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int a;
+
+    a=2;
+
+    while (a<=10){
+        printf("%d",a);
+        a=a+2;
+    }
+
+
+
+}
